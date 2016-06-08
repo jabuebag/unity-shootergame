1450e8218635e028847aa6d33d250c2adbbea14e
jabue <sdutyang@outlook.com>
2016-06-08 09:51:18 -0700

    code backup

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpdate () {
+		// Create a postion the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
+
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
